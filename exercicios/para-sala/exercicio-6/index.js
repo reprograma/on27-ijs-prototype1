3e6ac33ec3df3a3ac4a8a22374cda0b7e83ab34a
JOSEANE GUEDES <joseane_23@hotmail.com>
2023-09-26 17:07:10 -0300

    Add exercicio prototype

--- exercicios/para-sala/exercicio-6/index.js
+++ exercicios/para-sala/exercicio-6/index.js
@@ -1,7 +1,8 @@
 const { Account } = require('./resolucao');
 
-luaraAccount = Account(123, 0001, 1000);
+luaraAccount = Account(123, 0o1, 1000);
 luaraAccount.credit(2000);
-luaraAccount.debit(500);
-outraconta = Account(456, 0001, 100);
-luaraAccount.transferTo(outraconta, 100000);
+luaraAccount.debit(10000);
+
+outraconta = Account(456, 0o1, 100);
+luaraAccount.transferTo(outraconta, 500);
