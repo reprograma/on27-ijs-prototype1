e4461edd00abbd3059c756774bea9bfa8fccff22
Elis Coimbra <eliscoimbrademoura831@gmail.com>
2023-09-27 14:45:58 -0300

    Exercício Prototype Semana 2

--- exercicios/para-sala/exercicio-2/index.js
+++
@@ -1,9 +0,0 @@
-const { Employee } = require('./resolucao');
-
-const employee1 = Employee('Luara', 'Kerlen', 10000);
-console.log(employee1);
-console.log(employee1.raiseSalary(10));
-
-const employee2 = Employee('Maria', 'Santos', 20000);
-console.log(employee2);
-console.log(employee2.raiseSalary(5));
