64d8b2ce7f016704b94bfe41db7f74d2f45092a6
JOSEANE GUEDES <joseane_23@hotmail.com>
2023-09-27 16:30:11 -0300

    Add desafio contendo funções construtoras modelo Pseudoclassical Instantiation

---
+++ exercicios/joseane-guedes/index.js
@@ -0,0 +1,33 @@
+const { AnimalExotico, Cachorro, Gato } = require('./desafio');
+
+const gato = new Gato("Pedrita", 18, "preta", true);
+gato.miar();
+gato.historico.vacinar("gripe");
+gato.historico.consultar("30/9/2023");
+gato.historico.realizarProcedimento("ultrassom", "fratura");
+
+const cachorro = new Cachorro("Osho", 42, "marrom", false, "srd");
+cachorro.latir();
+cachorro.historico.vacinar("cinomose");
+cachorro.historico.consultar("15/9/2023");
+cachorro.historico.realizarProcedimento("raio-x", "fratura");
+
+const animal = new AnimalExotico("Shein", 4, "rosa", "hamster");
+console.log(`Shein é adulto? ${animal.ehAdulto()}`);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
