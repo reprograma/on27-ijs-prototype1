2b941bdf5b61f000afcc9120750d44076e5bec4b
JOSEANE GUEDES <joseane_23@hotmail.com>
2023-09-27 16:43:08 -0300

    Add codigo refatorado

--- exercicios/joseane-guedes/desafio.js
+++ exercicios/joseane-guedes/desafio.js
@@ -5,44 +5,30 @@ function Animal(nome, idade, cor) {
     this.historico = new HistoricoMedico();
 }
 
-// Função construtora para o objeto Gato
 function Gato(nome, idade, cor, castrado) {
     Animal.call(this, nome, idade, cor);
     this.castrado = castrado;
 }
 
-// Herda métodos de Animal
-Gato.prototype = Object.create(Animal.prototype);
-
-// Método específico para Gato
 Gato.prototype.miar = function () {
     console.log("Meow!");
 };
 
-// Função construtora para o objeto Cachorro
 function Cachorro(nome, idade, cor, castrado, raca) {
     Animal.call(this, nome, idade, cor);
     this.castrado = castrado;
     this.raca = raca;
 }
 
-// Herda métodos de Animal
-Cachorro.prototype = Object.create(Animal.prototype);
-
-// Método específico para Cachorro
 Cachorro.prototype.latir = function () {
     console.log("Au Au Au!");
 };
 
-// Função construtora para o objeto AnimalExotico
 function AnimalExotico(nome, idade, cor, especie) {
     Animal.call(this, nome, idade, cor);
     this.especie = especie;
 }
-// Herda métodos de Animal
-AnimalExotico.prototype = Object.create(Animal.prototype);
 
-// Método específico para AnimalExotico
 AnimalExotico.prototype.ehAdulto = function () {
     const idadeLimitePorEspecie = {
         hamster: 2,
@@ -59,20 +45,16 @@ function HistoricoMedico() {
     this.procedimentos = [];
 }
 
-// Método para adicionar uma nova vacina ao histórico
 HistoricoMedico.prototype.vacinar = function (novaVacina) {
     this.vacinas.push(novaVacina);
 };
 
-// Método para adicionar uma nova consulta ao histórico (em ordem cronológica reversa)
 HistoricoMedico.prototype.consultar = function (dataConsulta) {
     this.consultas.unshift(dataConsulta);
 };
 
-// Método para adicionar um novo procedimento ao histórico
 HistoricoMedico.prototype.realizarProcedimento = function (tipoProcedimento, motivoProcedimento) {
     this.procedimentos.push({ tipo: tipoProcedimento, motivo: motivoProcedimento });
 };
-
 
 module.exports = { AnimalExotico, Cachorro, Gato };
--- exercicios/joseane-guedes/index.js
+++ exercicios/joseane-guedes/index.js
@@ -6,13 +6,13 @@ gato.historico.vacinar("gripe");
 gato.historico.consultar("30/9/2023");
 gato.historico.realizarProcedimento("ultrassom", "fratura");
 
-const cachorro = new Cachorro("Osho", 42, "marrom", false, "srd");
+const cachorro = new Cachorro("Osho", 42, "marrom", false, "vira-lata caramelo");
 cachorro.latir();
 cachorro.historico.vacinar("cinomose");
 cachorro.historico.consultar("15/9/2023");
 cachorro.historico.realizarProcedimento("raio-x", "fratura");
 
-const animal = new AnimalExotico("Shein", 4, "rosa", "hamster");
+const animal = new AnimalExotico("Shein", 1, "rosa", "hamster");
 console.log(`Shein é adulto? ${animal.ehAdulto()}`);
 
 
